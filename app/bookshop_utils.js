46a17585d02b81c6f7e8c216ee107bfa62e8f7fc
Kinga <kczuchrowska@gmail.com>
2020-02-12 15:45:43 +0000

    working on exercises

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -1,7 +1,19 @@
 function addVAT(price) {
     return price + (0.2 * price);
 }
+function getFullName(name, surname){
+    return name + ' ' + surname;
+}
+function makeHalfPrice(price){
+        return price *0.5;
+}
+function countBooks(books){
+    books = [1, 2, 3]
+    return books.length;
 
+
+}
 module.exports = {
-    addVAT
+    addVAT, getFullName, makeHalfPrice, countBooks
 };
+
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,5 +1,5 @@
 const utils = require('../app/bookshop_utils');
-
+// first block for testing addVat
 describe("utils.addVAT", () => {
   test("returns 36 when passed 30", () => {
     expect(utils.addVAT(30)).toBe(36);
@@ -7,5 +7,26 @@ describe("utils.addVAT", () => {
   
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
+  });
+
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
+});
+// new block for testing makeHalfPrice
+describe("utils.getFullName", () => {
+  test("returns Kinga Czu", () => {
+    expect(utils.getFullName("Kinga", "Czu")).toBe("Kinga Czu");
+  });
+});
+
+describe("utils.makeHalfPrice", () => {
+  test("returns price with 50% discount", () => {
+    expect(utils.makeHalfPrice(100)).toBe(50);
+  });
+});
+describe("utils.countBooks", () => {
+  test("returns 2 when there are two books", () => {
+    expect(utils.countBooks["book1", "book2"]).toBe(2);
   });
 });
